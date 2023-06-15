1f2710f7f8a5f1ee266d03fd93fd75d8f97ba7b0
Vicente Barros <vmabarros@ua.pt>
2023-06-15 16:03:21 +0100

    Add argumment to pass the spectogram type

--- src/dataProcess.py
+++ src/dataProcess.py
@@ -1,15 +1,20 @@
+import argparse
 from sklearn.model_selection import train_test_split
 from audioProcessor import Audio, AudioProcessor
-import os
+import os, logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
-def main():
+
+def process_data(spectogram_type="mfcc"):
     ap = AudioProcessor()
 
     sanitized_audio_data, _ = ap.sanitize_audio()
-    spectogram_type = "mfcc"
 
     #Clean previous data
+    logger.info("Cleaning previous data")
     for file in os.listdir(f"data/spectograms/{spectogram_type}/train"):
         os.remove(f"data/spectograms/{spectogram_type}/train/{file}")
 
@@ -21,14 +26,17 @@ def main():
 
 
     #Convert sanitized_audio_data to spectrogram
+    logger.info("Converting to spectrogram")
     sanitized_audio_data = [Audio(ap.spectogram(audio.sample,spec_func=spectogram_type),audio.label) for audio in sanitized_audio_data]
 
     #Split between train and test and cross validation
 
+    logger.info("Splitting data")
     train, test = train_test_split(sanitized_audio_data, test_size=0.4, random_state=42,stratify=[audio.label for audio in sanitized_audio_data])
     train, cv = train_test_split(train, test_size=0.5, random_state=42,stratify=[audio.label for audio in train])
 
     # Save the data in the respective folders
+    logger.info("Saving data")
     for idx, audio in enumerate(train):
         ap.save_spectogram(audio.sample, f"label_{audio.label}_sample_{idx}",data_split="train",spectogram_type=spectogram_type)
     for idx, audio in enumerate(test):
@@ -36,6 +44,10 @@ def main():
     for idx,audio in enumerate(cv):
         ap.save_spectogram(audio.sample,f"label_{audio.label}_sample_{idx}",data_split="cv",spectogram_type=spectogram_type)
         
+    logger.info("Done")
 
 if __name__ == "__main__":
-    main()
+    argParser = argparse.ArgumentParser(description="Process audio files")
+    argParser.add_argument("--spec", type=str, default="mfcc", help="Type of spectogram to use")
+    args = argParser.parse_args()
+    process_data(args.spec)
